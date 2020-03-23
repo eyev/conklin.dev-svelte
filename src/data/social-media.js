bdce53ac93924d6f8de98b43bc1dbe44193b7701
Justin Conklin <jconklin@parkplacetech.com>
2020-03-23 01:43:52 -0400

    feat(socialMedia): add social media definitions

---
+++ src/data/social-media.js
@@ -0,0 +1,26 @@
+import {
+  faGithub,
+  faLinkedin,
+  faTwitter,
+} from '@fortawesome/free-brands-svg-icons';
+
+export const SOCIAL_MEDIA = [
+  {
+    id: 'linkedin',
+    icon: faLinkedin,
+    name: 'LinkedIn',
+    route: 'https://www.linkedin.com/in/',
+  },
+  {
+    id: 'github',
+    icon: faGithub,
+    name: 'GitHub',
+    route: 'https://github.com/',
+  },
+  {
+    id: 'twitter',
+    icon: faTwitter,
+    name: 'Twitter',
+    route: 'https://twitter.com/',
+  },
+];
