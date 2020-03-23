5bf09e9cb97ad26a279e96895cc1a11be9aa5b48
Justin Conklin <jconklin@parkplacetech.com>
2020-03-23 01:53:37 -0400

    fix(index): add new built out components

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,52 +1,26 @@
-<style>
-  h1,
-  figure,
-  p {
-    text-align: center;
-    margin: 0 auto;
-  }
-
-  h1 {
-    font-size: 2.8em;
-    text-transform: uppercase;
-    font-weight: 700;
-    margin: 0 0 0.5em 0;
-  }
-
-  figure {
-    margin: 0 0 1em 0;
-  }
-
-  img {
-    width: 100%;
-    max-width: 400px;
-    margin: 0 0 1em 0;
-  }
-
-  p {
-    margin: 1em auto;
-  }
-
-  @media (min-width: 480px) {
-    h1 {
-      font-size: 4em;
+<script context="module">
+  export async function preload({ params, query }) {
+    const profileRequest = await this.fetch(`/data/webume.json`);
+    const profile = await profileRequest.json();
+    if (profileRequest.status === 404) {
+      this.error(404, 'Not found');
     }
+    return { profile };
   }
-</style>
+</script>
+
+<script>
+  import SuperHero from '../components/SuperHero.svelte';
+  import Experience from '../components/Experience.svelte';
+  import Technologies from '../components/Technologies.svelte';
+
+  export let profile;
+</script>
 
 <svelte:head>
-  <title>Sapper project template</title>
+  <title>conklin.dev - Justin Conklin - Software Engineer Portfolio</title>
 </svelte:head>
-
-<h1>Great success!</h1>
-
-<figure>
-  <img alt="Borat" src="great-success.png" />
-  <figcaption>HIGH FIVE!</figcaption>
-</figure>
-
-<p>
-  <strong>
-    Try editing this file (src/routes/index.svelte) to test live reloading.
-  </strong>
-</p>
+<SuperHero {profile} />
+<Experience {profile} />
+<Technologies {profile} />
+<!-- TODO: Projects? -->
