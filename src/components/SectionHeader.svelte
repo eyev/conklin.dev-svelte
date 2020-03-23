e1da729198270c98ff0b6867d21357c3e56b873d
Justin Conklin <jconklin@parkplacetech.com>
2020-03-23 01:52:24 -0400

    feat(SectionHeader): build out generic Header component

---
+++ src/components/SectionHeader.svelte
@@ -0,0 +1,13 @@
+<style>
+  h2 {
+    font-size: 2.375rem;
+    margin-bottom: 1rem;
+    font-family: 'Raleway', 'Lucida Sans', 'Lucida Sans Regular',
+      'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif;
+    font-weight: 700;
+  }
+</style>
+
+<h2>
+  <slot />
+</h2>
