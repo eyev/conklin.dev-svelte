cfddb070753d7c3d8a1757259caad6f3e95e46f3
Justin Conklin <jconklin@parkplacetech.com>
2020-03-23 01:09:18 -0400

    feat(Tooltip): build out generic tooltip component TODO: this could be better, move away from css solution

---
+++ src/components/Tooltip.svelte
@@ -0,0 +1,38 @@
+<script>
+  export let content;
+</script>
+
+<style>
+  .tooltip {
+    position: relative;
+    display: inline-block;
+  }
+
+  .tooltip:hover .tooltiptext {
+    visibility: visible;
+  }
+
+  .tooltip .tooltiptext {
+    visibility: hidden;
+    width: 6rem;
+    background-color: var(--tooltip-background);
+    color: var(--tooltip-color);
+    text-align: center;
+    padding: 0.5rem 0;
+    border-radius: 0.25rem;
+    border: 1px solid rgba(255, 255, 255, 0.1);
+    position: absolute;
+    z-index: 1;
+  }
+
+  .tooltip .tooltiptext {
+    top: 80%;
+    left: 50%;
+    margin-left: -3.5rem;
+  }
+</style>
+
+<div class="tooltip">
+  <slot />
+  <span class="tooltiptext">{content}</span>
+</div>
