eb12299f942ed9aa70422d48f025c399955c4376
Justin Conklin <jconklin@parkplacetech.com>
2020-03-23 01:52:00 -0400

    feat(Header): build out Header component

---
+++ src/components/Header.svelte
@@ -0,0 +1,36 @@
+<script>
+  import Nav from '../components/Nav.svelte';
+  import ThemeToggle from './ThemeToggle.svelte';
+
+  export let segment;
+</script>
+
+<style>
+  header {
+    display: flex;
+    padding: 0.5rem 0;
+    align-items: center;
+    margin-bottom: 2.5rem;
+    position: -webkit-sticky;
+    position: sticky;
+    top: 0;
+    background: #1f1f21;
+    background: var(--body-background);
+  }
+  .logo {
+    font-size: 1.5rem;
+    font-weight: 700;
+    font-family: 'Open Sans', sans-serif;
+    color: #fff;
+    color: var(--logo-color);
+    display: flex;
+    margin-right: auto;
+    text-decoration: none;
+  }
+</style>
+
+<header>
+  <a href="#top" class="logo">conklin.dev</a>
+  <Nav />
+  <ThemeToggle />
+</header>
