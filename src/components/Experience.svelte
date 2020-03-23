effe9cd021916c94befc0ef7bdcb50126e267d5c
Justin Conklin <jconklin@parkplacetech.com>
2020-03-23 01:51:51 -0400

    feat(Experience): build out experience component

---
+++ src/components/Experience.svelte
@@ -0,0 +1,23 @@
+<script>
+  import SectionHeader from './SectionHeader.svelte';
+  import Timeline from './Timeline.svelte';
+  import TimelinePosition from './TimelinePosition.svelte';
+
+  export let profile;
+</script>
+
+<style>
+  section {
+    padding: 2rem 0;
+    margin-bottom: 4rem;
+  }
+</style>
+
+<section id="experience">
+  <SectionHeader>Experience</SectionHeader>
+  <Timeline>
+    {#each profile.experience as position}
+      <TimelinePosition {position} />
+    {/each}
+  </Timeline>
+</section>
